6ca7c4075d352c817b2b25d88a0b752b127f8790
RobWar-code <rowaniar@aol.com>
2023-06-15 18:37:34 +0000

    Add StatefulGreeting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import FunctionalGreeting from './Components/FunctionalGreeting.js';
-import FunctionalGreetingWithProps from './Components/FunctionalGreetingWithProps';
+import FunctionalGreetingWithProps from './Components/FunctionalGreetingWithProps.js';
+import StatefulGreeting from './Components/StatefulGreeting.js'
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" />
+      <StatefulGreeting />
     </div>
   );
 }
---
+++ my-app/src/Components/StatefulGreeting.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class StatefulGreeting extends React.Component{
+
+    render () {
+        return <h1>Hello from the StatefulGreeting Component</h1>
+    }
+}
+
+export default StatefulGreeting;
