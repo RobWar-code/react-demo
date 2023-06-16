bbad883290ebc59f90682793889ca294f91675ee
RobWar-code <rowaniar@aol.com>
2023-06-16 08:54:54 +0000

    Begin StatefulGreeting

--- my-app/src/Components/StatefulGreeting.js
+++ my-app/src/Components/StatefulGreeting.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class StatefulGreeting extends React.Component{
+    constructor (props) {
+        super(props);
+        state = {
+            introduction: "Hello!",
+            buttonText: "Exit"
+        };
+    }
 
     render () {
-        return <h1>Hello {this.props.message}</h1>
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.message}</h1>
+                <button>{this.state.buttonText}</button>
+            </div>
+        )
     }
 }
 
