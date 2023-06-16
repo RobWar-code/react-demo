a1b9f62b90c1b677a334c456813850ea708a4521
RobWar-code <rowaniar@aol.com>
2023-06-16 19:59:15 +0000

    Add callback

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import FunctionalGreeting from './Components/FunctionalGreeting.js';
 import FunctionalGreetingWithProps from './Components/FunctionalGreetingWithProps.js';
-import StatefulGreeting from './Components/StatefulGreeting.js'
+import StatefulGreeting from './Components/StatefulGreeting.js';
+import StatefulGreetingWithCallback from './Components/StatefulGreetingWithCallback.js';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" />
       <StatefulGreeting message="from Stateful Class" />
+      <StatefulGreetingWithCallback message="from StatefulGreetingWithCallback" />
     </div>
   );
 }
