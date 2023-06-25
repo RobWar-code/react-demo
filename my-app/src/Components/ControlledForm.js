1dc604fd7a2557f5e1a9c3cd8ff9ec1afb7c7e69
RobWar-code <rowaniar@aol.com>
2023-06-25 19:15:36 +0000

    Add controlled form

--- my-app/src/Components/ControlledForm.js
+++ my-app/src/Components/ControlledForm.js
@@ -5,12 +5,38 @@ export class ControlledForm extends Component {
     super(props)
   
     this.state = {
-       name: ""
+       name: "",
+       queryType: "website",
+       comments: ""
     }
   }
 
+  /*
   handleNameChange = (event) => {
     this.setState({name: event.target.value});
+  }
+
+  handleQueryTypeChange = (event) => {
+    this.setState({queryType: event.target.value})
+  } 
+
+  handleCommentChange = (event) => {
+    this.setState({comment: event.target.value})
+  }
+  */
+
+  // The following replaces the methods above
+  handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // Include javascript here to handle the state data
+    // set by the form and handlers
   }
 
   render() {
@@ -20,9 +46,24 @@ export class ControlledForm extends Component {
            However, we can make it controlled by referring to state
            elements and using a change event handler
         */}
-        <form>
-            <label htmlFor="id-name">Your Name:</label>
-            <input id="id-name" name="name" type="text" value={this.state.name} onChange={this.handleNameChange}/>
+        <form onSubmit={this.handleSubmit}>
+            <h2>Please Fill Out the form Below</h2>
+            <div>
+              <label htmlFor="id-name">Your Name:</label>
+              <input id="id-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+            </div>
+            <div>
+            <label htmlFor="id-query">Query Type:</label>
+              <select id="id-query" name="queryType" value={this.state.queryType} onChange={this.handleChange}>
+                <option value="website">Website Issue</option>
+                <option value="order">Order Enquiry</option>
+                <option value="general">General Issue</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="id-comments">Comments:</label>
+              <textarea id="id-comments" name="comments" value={this.state.comments} onChange={this.handleChange}/>
+            </div>
             <input type="submit" value="Submit" />
         </form>
       </div>
